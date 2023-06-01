a74263987e9c1a47a2ab0bd04861aadc5b9362a8
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2023-06-01 15:10:39 -0400

    update message

--- src/strings.helper.ts
+++ src/strings.helper.ts
@@ -1,6 +1,6 @@
 export default {
     summary: (firstName, hours, from, to) => `Almost there! So far, ${firstName} has entered ${hours} hours for the week of ${from} to ${to}.`,
     missingHours: (hours) => `${40 - hours} hours logged`,
-    withAttachments: (from, to) => `*Almost there!* These team members still have hours to enter for the week of ${from} to ${to}.`,
+    withAttachments: (from, to) => `*Hello!* These are the logged hours so far for the week of ${from} to ${to}. Please make sure they are up-to-date :robot_face:`,
     withoutAttachments: () => '*Nicely done, folks!* I’ve got no reminders for you, because *everyone has already entered their hours.* Keep it up!'
   };
